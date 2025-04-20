3ff2d1273cc21063711392a567854453193f5b77
fatmakhadidja <154927228+fatmakhadidja@users.noreply.github.com>
2025-04-20 20:16:13 +0100

    Frontend basically done

--- voice_translator/lib/screens/home_screen.dart
+++ voice_translator/lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:voice_translator/utils/constants.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,8 +9,168 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // Selected tab index
+  int _selectedIndex = 0;
+
+  // Widgets for each tab
+  final List<Widget> _widgets = [
+    const TextTranslation(key: ValueKey(0)), // Unique key for AnimatedSwitcher
+    const VoiceTranslation(key: ValueKey(1)),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: bgColor,
+      resizeToAvoidBottomInset:
+          true, // Automatically resize to avoid keyboard overflow
+      body: SafeArea(
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          switchInCurve: Curves.easeIn,
+          switchOutCurve: Curves.easeOut,
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+          child: _widgets[_selectedIndex],
+        ),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(15),
+          topRight: Radius.circular(15),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: lightGrey,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.text_fields),
+              label: 'Text Translation',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.voice_chat),
+              label: 'Voice Translation',
+            ),
+          ],
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
+
+class TextTranslation extends StatefulWidget {
+  const TextTranslation({super.key});
+
+  @override
+  State<TextTranslation> createState() => _TextTranslationState();
+}
+
+class _TextTranslationState extends State<TextTranslation> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                MyText(
+                  couleur: Colors.black,
+                  fontfamily: 'Viga',
+                  fontsize: 24,
+                  fontweight: FontWeight.w400,
+                  text: 'Global Voice',
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TranslationTextContainer(size: 0.33, linesNumber: 6),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TranslationTextContainer(size: 0.33, linesNumber: 6),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                BorderedButton(buttomText: 'English'),
+                Icon(Icons.arrow_right_alt_outlined, color: lightGrey),
+                BorderedButton(buttomText: 'Arabic'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VoiceTranslation extends StatefulWidget {
+  const VoiceTranslation({super.key});
+
+  @override
+  State<VoiceTranslation> createState() => _VoiceTranslationState();
+}
+
+class _VoiceTranslationState extends State<VoiceTranslation> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                MyText(
+                  couleur: Colors.black,
+                  fontfamily: 'Viga',
+                  fontsize: 24,
+                  fontweight: FontWeight.w400,
+                  text: 'Global Voice',
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: VoiceTranslationContainer(),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TranslationTextContainer(size: 0.46, linesNumber: 10),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                BorderedButton(buttomText: 'Auto'),
+                Icon(Icons.arrow_right_alt_outlined, color: lightGrey),
+                BorderedButton(buttomText: 'English'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- voice_translator/lib/utils/constants.dart
+++ voice_translator/lib/utils/constants.dart
@@ -7,8 +7,6 @@ const purple = Color(0xff4340CB);
 const pink = Color(0xffCB40B5);
 const orange = Color(0xffDE643E);
 
-
-
 TextStyle gradientStyle = TextStyle(
   fontFamily: 'Viga',
   fontWeight: FontWeight.w400,
@@ -90,6 +88,212 @@ class ContinueButton extends StatelessWidget {
   }
 }
 
+class TranslationTextContainer extends StatefulWidget {
+  final double size;
+  final int linesNumber;
+  const TranslationTextContainer({
+    super.key,
+    required this.size,
+    required this.linesNumber,
+  });
 
+  @override
+  State<TranslationTextContainer> createState() =>
+      _TranslationTextContainerState();
+}
 
+class _TranslationTextContainerState extends State<TranslationTextContainer> {
+  final TextEditingController _controller = TextEditingController();
 
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      height: MediaQuery.of(context).size.height * widget.size,
+
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            offset: const Offset(4, 4),
+            blurRadius: 10,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 5, 0, 0),
+            child: MyText(
+              couleur: darkGrey,
+              fontfamily: 'Viga',
+              fontsize: 12,
+              fontweight: FontWeight.w400,
+              text: 'English',
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+            child: TextField(
+              controller: _controller,
+              keyboardType: TextInputType.multiline,
+              maxLines: widget.linesNumber,
+              decoration: const InputDecoration(
+                hintText: "Enter text here ..",
+                hintStyle: TextStyle(fontFamily: 'Viga', color: lightGrey),
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                isCollapsed: true,
+              ),
+              style: const TextStyle(fontFamily: 'Viga', color: darkGrey),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.volume_up_sharp, color: lightGrey, size: 25),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BorderedButton extends StatefulWidget {
+  final String buttomText;
+  const BorderedButton({super.key, required this.buttomText});
+
+  @override
+  State<BorderedButton> createState() => _BorderedButtonState();
+}
+
+class _BorderedButtonState extends State<BorderedButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // Gradient border container
+        Container(
+          width: MediaQuery.of(context).size.width * 0.4,
+          padding: EdgeInsets.all(2), // Border thickness
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.purple, Colors.pink, Colors.orange],
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white, // Button background
+              borderRadius: BorderRadius.circular(18),
+            ),
+            child: OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                side:
+                    BorderSide
+                        .none, // No visible side (since border handled by gradient)
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: MyText(
+                couleur: Colors.black,
+                fontfamily: 'Viga',
+                fontsize: 16,
+                fontweight: FontWeight.w400,
+                text: widget.buttomText,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class VoiceTranslationContainer extends StatefulWidget {
+  const VoiceTranslationContainer({super.key});
+
+  @override
+  State<VoiceTranslationContainer> createState() =>
+      _VoiceTranslationContainerState();
+}
+
+class _VoiceTranslationContainerState extends State<VoiceTranslationContainer> {
+  bool isRecording = false; // This keeps track of mic state
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      height: MediaQuery.of(context).size.height * 0.2,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            offset: const Offset(4, 4),
+            blurRadius: 10,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            MyText(
+              couleur: isRecording ? Colors.red : darkGrey,
+              fontfamily: 'Viga',
+              fontsize: 17,
+              fontweight: FontWeight.w500,
+              text: isRecording ? 'Recording...' : 'Record here ..',
+            ),
+            Center(
+              child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    isRecording = !isRecording;
+                  });
+                },
+                icon: Icon(
+                  isRecording ? Icons.stop_circle_outlined : Icons.mic,
+                  size: 50,
+                  color: isRecording ? Colors.red : darkGrey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
